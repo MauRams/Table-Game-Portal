d7f43f99eff06b99dff19f12e7df0b2fdde27277
Deniss <denmantm@inbox.lv>
2015-12-07 13:05:16 +0000

    styling login page

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 Running.....
 
 
-  1. First please delete node_modules directory
+  1. First please delete node_modules directory from root
   2. Please type in console 'NPM install'
   3. Downgrade to node v0.10.XX (console: 'NVM install 0.10.12')
   4. Type command in console NPM install node_xslt
-  5. Run by typing in console node server
+  5. Run by typing in console 'node server'
 
 
 
---
+++ public/js/running_text_login.js
@@ -0,0 +1,38 @@
+//RUNNING TEXT SCRIPT
+//Code snippet from: http://jsfiddle.net/ARTsinn/RED9H/2/
+$( document ).ready(function() {
+$.fn.typer = function (text, options) {
+    options = $.extend({}, {
+        delay: 1000,
+        duration: 10000,
+        easing: 'linear',
+        endless: false
+    }, options);
+    var elem = $(this);
+    (function loop(i) {
+        var e = typeof text === 'string' ? text : text[i];
+        // strip html tags, might be helpful
+        // e.replace(/(<.*?>)/ig,"")
+        $({len: 0}).delay(options.delay).animate({len: e.length}, {
+            duration: options.duration,
+            easing: options.easing,
+            step: function (now) {
+                var pos = Math.ceil(now),
+                    char = e.substr(pos, 0);
+                elem.html(e.substr(0, pos));
+            },
+            complete: function () {
+                i++;
+                if (i === text.length && !options.endless) {
+                    return;
+                } else if (i === text.length) {
+                    i = 0;
+                }
+                loop(i);
+            }
+        });
+    })(0);
+};
+$('#bannerText').typer(['<b style="color:red; font-weight:bold">Nicest</b> <u style="color:white">Ever Table</u> <i style="color:yellow;font-weight:bold" >Portal Web Site</i>']);
+setInterval(function(){ $('#bannerText').typer(['<b style="color:red; font-weight:bold">Nicest</b> <u style="color:white">Ever Table</u> <i style="color:yellow;font-weight:bold" >Portal Web Site</i>']); }, 25000);
+});

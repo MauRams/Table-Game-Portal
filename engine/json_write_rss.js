6cea19b2d90db47dbeed0a29b2eab453647db432
Deniss <denmantm@inbox.lv>
2015-12-12 11:31:14 +0000

    rss

---
+++ engine/json_write_rss.js
@@ -0,0 +1,40 @@
+//param is the argument which adds +1 to certain game..
+module.exports = function(param){
+    //reading json file from disk and assigning it
+    // this will update rss file
+  var readJson = require('../engine/json_read.js')('./jsonStorage/rss.json'),
+        fs = require('fs');
+  
+  
+  if(param =='game1'){
+      
+      var stringValue = readJson.rss[0].description,
+      
+     // getting current value and parsing it
+      short = parseInt(stringValue.substr(29));
+      short+=1;
+      var ss = 'Avalable, games played total:'+short;
+      readJson.rss[0].description = ss;
+      
+      
+      
+      
+      
+      var JSONformated = JSON.stringify(readJson, null, 4);
+      fs.writeFileSync('./jsonStorage/rss.json', JSONformated);
+      
+      
+  }
+  else if(param == 'game2'){
+        var stringValue = readJson.rss[1].description,
+        short = parseInt(stringValue.substr(29));
+        var ss = 'Avalable, games played total:'+short;
+              short+=1;
+        readJson.rss[1].description = ss;
+        
+      var JSONformated = JSON.stringify(readJson, null, 4);
+      fs.writeFileSync('../jsonStorage/rss.json', JSONformated);
+      
+    }
+    
+};
--- engine/xslt_transform.js
+++ engine/xslt_transform.js
@@ -1,10 +1,9 @@
 var xslt = require('node_xslt');
 module.exports = function(transform,paths){
-  		
-		                                           
+	
+	
+	
 		var stylesheet = xslt.readXsltFile(transform);                          
 		var document = xslt.readXmlFile(paths);                                                                                                          
-		
 		return xslt.transform(stylesheet, document, []);
-
 };
